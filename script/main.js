bbb574e6fcc364654154397b70af1cab61394a4e
Suda Mikihiro <s1711373@coins.tsukuba.ac.jp>
2017-08-30 11:54:04 +0900

    レイアウト修正

--- script/main.js
+++ script/main.js
@@ -13,15 +13,13 @@ $(window).resize(function () {
     if ($(window).width() / $(window).height() < 1) {
         $("div.centering").css({
             "font-size": "15vh",
-            "letter-spacing": "-0.2em",
             "-webkit-writing-mode": "vertical-rl",
             "-ms-writing-mode": "tb-rl",
             "writing-mode": "vertical-rl"
         });
     } else {
         $("div.centering").css({
-            "font-size": "15vw",
-            "letter-spacing": "0",
+            "font-size": "13vw",
             "-webkit-writing-mode": "horizontal-tb",
             "-ms-writing-mode": "lr-tb",
             "writing-mode": "horizontal-tb"
