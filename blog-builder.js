fe94f42de982afdf1ba69be8c7f117f98ddcf6b5
sudame <su.glasses.923@gmail.com>
2017-12-23 16:59:13 +0900

    トップページ作成

--- blog-builder.js
+++ blog-builder.js
@@ -2,15 +2,21 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 
 const blogSrc = './blog/';
-fs.readdirSync(`${blogSrc}/src/contents`).forEach((p) => {
-    if (p.match(/.*.html/)) {
-        const $global = cheerio.load(fs.readFileSync('./blog/src/global/global.html', 'utf-8'), {
-            decodeEntities: false
-        });
+const $index = cheerio.load(fs.readFileSync('./blog/index.html', 'utf-8'), { decodeEntities: false });
+$index('.content').text('');
+
+fs.readdirSync(`${blogSrc}/src/contents`).sort((a,b) => {
+    a = a.toString().toLowerCase();
+    b = b.toString().toLowerCase();
+    if(a > b) return -1;
+    else if(a < b) return 1;
+    else return 0;
+}).forEach((p) => {
+if (p.match(/.*.html/) && !p.match(/_.*/)) {
+        const $global = cheerio.load(fs.readFileSync('./blog/src/global/global.html', 'utf-8'), { decodeEntities: false });
         const c = fs.readFileSync(`${blogSrc}/src/contents/${p}`, 'utf-8');
-        const $contents = cheerio.load(c, {
-            decodeEntities: false
-        });
+        const s = fs.statSync(`${blogSrc}/src/contents/${p}`);
+        const $contents = cheerio.load(c, { decodeEntities: false });
         if ($contents('style')) {
             $contents('style').each((i, v) => {
                 $global('head').append(v);
@@ -26,5 +32,28 @@ fs.readdirSync(`${blogSrc}/src/contents`).forEach((p) => {
         };
         $global('.content').append($contents('.content').html());
         fs.writeFileSync(blogSrc + p, $global.html());
+
+        $index('.content').append(`
+        <div class="article-card">
+            <div class="article-card__title">
+                <h3>${$contents('title').text()}</h3>
+            </div>
+            <div class="article-card__date">
+                <p>${$contents('meta[property="date"]').attr('content') || ''}</p>
+            </div>
+            <div class="article-card__content">
+                <p>
+                    ${$contents('.content section:not(.sub) p').text().slice(0, 150)}
+                </p>
+            </div>
+            <div class="article-card__more">
+                <a href="http://sudame.net/blog/${p}">
+                    記事を読む
+                </a>
+            </div>
+        </div>
+        `);
+
+        fs.writeFileSync('./blog/index.html', $index.html());
     }
 });
