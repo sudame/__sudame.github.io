35cf0cb2ac2a33c16e0a6fb568da905c8b7f378f
sudame <su.glasses.923@gmail.com>
2017-12-23 16:18:34 +0900

    変数のスコープミスを修正

--- blog-builder.js
+++ blog-builder.js
@@ -1,13 +1,12 @@
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-const $global = cheerio.load(fs.readFileSync('./blog/src/global/global.html', 'utf-8'), {
-    decodeEntities: false
-});
-
 const blogSrc = './blog/';
 fs.readdirSync(`${blogSrc}/src/contents`).forEach((p) => {
     if (p.match(/.*.html/)) {
+        const $global = cheerio.load(fs.readFileSync('./blog/src/global/global.html', 'utf-8'), {
+            decodeEntities: false
+        });
         const c = fs.readFileSync(`${blogSrc}/src/contents/${p}`, 'utf-8');
         const $contents = cheerio.load(c, {
             decodeEntities: false
@@ -17,7 +16,7 @@ fs.readdirSync(`${blogSrc}/src/contents`).forEach((p) => {
                 $global('head').append(v);
             })
         }
-        if($contents('title')) {
+        if ($contents('title')) {
             $global('meta[property="og:site_name"]').attr('content', $contents('title').text());
             $global('meta[property="og:title"]').attr('content', $contents('title').text());
             $global('meta[property="og:url"]').attr('content', `http://sudame.net/blog/${p}`);
